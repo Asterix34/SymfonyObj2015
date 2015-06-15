c2bf1fec790cc66acdea7338b42f1a2c28eaab33
Asterix34 <bclerc74@gmail.com>
2015-06-15 10:59:22 +0200

    CRUD Article

--- src/Stephane/BlogBundle/Controller/BlogController.php
+++ src/Stephane/BlogBundle/Controller/BlogController.php
@@ -3,65 +3,59 @@
 namespace Stephane\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Stephane\BlogBundle\Entity\Article;
 use Stephane\BlogBundle\Entity\Image;
 use Stephane\BlogBundle\Entity\Commentaire;
 use Stephane\BlogBundle\Entity\Categorie;
 use Stephane\BlogBundle\Form\ArticleType;
 
-class BlogController extends Controller
-{
-    public function indexAction($page)
-    {
-        $repository  = $this->getDoctrine()
-        					->getManager()
-        					->getRepository('StephaneBlogBundle:Article');
+class BlogController extends Controller {
+
+    public function indexAction($page) {
+        $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('StephaneBlogBundle:Article');
         //$listeArticles = $repository->findall();
-    	$listeArticles = $repository->getArticles();
-        return $this->render('StephaneBlogBundle:Blog:index.html.twig',
-    			array('page' => $page,
-    			'listeArticles' => $listeArticles));
+        $listeArticles = $repository->getArticles();
+        return $this->render('StephaneBlogBundle:Blog:index.html.twig', array('page' => $page,
+                    'listeArticles' => $listeArticles));
     }
-    
-	public function voirAction($id)
-    {
-        $repository  = $this->getDoctrine()
-        					->getManager()
-        					->getRepository('StephaneBlogBundle:Article');
+
+    public function voirAction($id) {
+        $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('StephaneBlogBundle:Article');
         $article = $repository->getArticle($id);
-        
-        $repository2  = $this->getDoctrine()
-        					->getManager()
-        					->getRepository('StephaneBlogBundle:Commentaire');
-        
-        $commentaires = $repository2->findBy(array('article' => $article),
-        								array('date' => 'desc'));
-        
+
+        $repository2 = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('StephaneBlogBundle:Commentaire');
+
+        $commentaires = $repository2->findBy(array('article' => $article), array('date' => 'desc'));
+
         $commentaires = $article->getCommentaires();
-        								
-    	return $this->render('StephaneBlogBundle:Blog:voir.html.twig', array('article' => $article
-    						, 'commentaires' => $commentaires));
+
+        return $this->render('StephaneBlogBundle:Blog:voir.html.twig', array('article' => $article
+                    , 'commentaires' => $commentaires));
     }
-    
-	public function ajouterAction()
-    {
+
+    public function ajouterAction() {
         $article = new Article;
-        
+
         $form = $this->createForm(new ArticleType, $article);
-        
+
         $request = $this->getRequest();
         if ($request->getMethod() == "POST") {
-        	$form->handleRequest($request);
-        	
-        	if ($form->isValid()) {
-        		$entityManager = $this->getDoctrine()->getManager();
-       			$entityManager->persist($article);
-       			$entityManager->flush();	
-        	}
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($article);
+                $entityManager->flush();
+            }
         }
-        
-        
+
+
 //        $formBuilder = $this->createFormBuilder($article);
 //        $formBuilder
 //        	->add('titre' , 'text')
@@ -70,7 +64,6 @@ class BlogController extends Controller
 //        	->add('datecreation', 'datetime')
 //        	->add('publication', 'checkbox');
 //        $form = $formBuilder->getForm();
-        	
 //        $image = new Image;
 //        $image->setSrc("tof1.jpg");
 //        $image->setAlt("tof1");
@@ -100,14 +93,12 @@ class BlogController extends Controller
 //        		->setImage($image)
 //        		->addCategory($categorie)
 //        		->addCategory($categorie1);
-                
 //        $em = $this->getDoctrine()->getManager();  // entity manager
 //        $em->persist($commentaire);
 //        $em->persist($commentaire1);
 //        $em->persist($categorie);
 //        $em->persist($categorie1);
 //        $em->persist($article);
-    	
 //        $article2 = new Article;
 //        $image2 = new Image;
 //        $image2->setSrc("tof2.jpg");
@@ -118,41 +109,36 @@ class BlogController extends Controller
 //        		->setImage($image2);
 //        $em->persist($article2);
 //        $em->flush();
-        
 //        return $this->render('StephaneBlogBundle:Blog:ajouter.html.twig'
 //        	,array('article' => $article, 'article2' => $article2));
         return $this->render('StephaneBlogBundle:Blog:ajouter.html.twig'
-        	,array('form' => $form->createView()));
+                        , array('form' => $form->createView()));
     }
-    
-	public function modifierAction($id)
-    {
+
+    public function modifierAction($id) {
 //        $em = $this->getDoctrine()->getManager();
-    	$repository  = $this->getDoctrine()
-        					->getManager()
-        					->getRepository('StephaneBlogBundle:Article');
+        $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('StephaneBlogBundle:Article');
         $article = $repository->find($id);
         $article->setAuteur('modifman');
 //        $em->persist($article); // inutile
 //        $em->flush();
-    	//return $this->render('StephaneBlogBundle:Blog:modifier.html.twig', array('article' => $article));
-    	return $this->redirect($this->generateUrl('stephane_blog_voir', array('id' => $article->getId())));
+        //return $this->render('StephaneBlogBundle:Blog:modifier.html.twig', array('article' => $article));
+        return $this->redirect($this->generateUrl('stephane_blog_voir', array('id' => $article->getId())));
     }
-    
-	public function menuAction()
-    {
+
+    public function menuAction() {
 //        $articles = array(
 //        	array('titre'=>'hello world 1', 'contenu'=>'Lorem ipsum dolor'),
 //        	array('titre'=>'hello world 2', 'contenu'=>'Lorem ipsum dolor'),
 //        	array('titre'=>'hello world 3', 'contenu'=>'Lorem ipsum dolor')
 //        );
-		$repository  = $this->getDoctrine()
-        					->getManager()
-        					->getRepository('StephaneBlogBundle:Article');
-        $articles = $repository->findBy(array('publication' => 1),
-        								array('datecreation' => 'desc'),
-        								3,0);
-    	return $this->render('StephaneBlogBundle:Blog:menu.html.twig',
-    				array('articles' => $articles));
+        $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('StephaneBlogBundle:Article');
+        $articles = $repository->findBy(array('publication' => 1), array('datecreation' => 'desc'), 3, 0);
+        return $this->render('StephaneBlogBundle:Blog:menu.html.twig', array('articles' => $articles));
     }
+
 }
